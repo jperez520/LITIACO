c04e5937d5df1584e39b4fc82a2057c305843f97
jperez520 <132482373+jperez520@users.noreply.github.com>
2025-10-26 04:14:32 -0500

    Update LITIACO.py

--- LITIACO.py
+++ LITIACO.py
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1" />
-  <title>Línea de tiempo — Abusos del clero (Pantalla completa)</title>
+  <title>Línea de tiempo Abusos del clero (Pantalla completa)</title>
   <style>
     :root{
       --bg:#f7efe6; /* papel antiguo */
